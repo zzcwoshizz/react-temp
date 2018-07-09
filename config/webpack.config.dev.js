bc51e036cda4011d5ff559ec46e1fc926ccaab95
张志超 <zzc1026503117zzc@gmail.com>
2018-07-09 12:02:02 +0800

    devtool eval map

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -24,6 +24,7 @@ module.exports = {
     publicPath: publicPath,
     path: paths.appBuild,
   },
+  devtool: 'eval',
   module: {
     rules: [
       {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -145,6 +145,18 @@ module.exports = {
       inject: true,
       template: path.resolve(paths.appPublic, 'index.html'),
       filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
     }),
     new AutoDllPlugin({
       debug: true,
